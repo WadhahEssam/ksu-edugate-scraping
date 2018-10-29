1978e60f50c3249474b8431d8dcd6758ade70ff1
Wadah Esam <wadah-wleed@hotmail.com>
2018-10-29 15:33:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.post('/getStudentInformation', async (req, res) => {
   try {
     let openPages = await browser.pages();
     while(openPages.length !== 1) {
-      await sleep(200 * openPages.length);
+      await sleep(200 + ( 50 * openPages.length) );
       openPages = await browser.pages();
     }
 
